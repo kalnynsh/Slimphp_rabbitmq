69cd52c875202ffba05a68bec902c8b9af2a41ce
kalnynsh <kda869@yandex.ru>
2019-07-16 20:08:44 +0300

    Added to frontend Author  and Profile.

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -14,8 +14,13 @@
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item"><router-link class="nav-link" :to="{name: 'home'}">Home</router-link></li>
-          <li class="nav-item"><router-link class="nav-link" :to="{name: 'about'}">About</router-link></li>
           <template v-if="$store.getters.isLoggedIn">
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{name: 'profile'}">Profile</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{name: 'author'}">Author</router-link>
+            </li>
             <li class="nav-item">
               <a href="" class="nav-link" @click="logout">Log Out</a>
             </li>
