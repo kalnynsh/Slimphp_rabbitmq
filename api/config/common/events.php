82ac36f582dde77b018ec291ddf31f987e175fe7
kalnynsh <kda869@yandex.ru>
2019-07-22 18:23:35 +0300

    Added VideoCreatedListener with AmqpStreamConnection

--- api/config/common/events.php
+++ api/config/common/events.php
@@ -2,29 +2,38 @@
 
 declare(strict_types=1);
 
-use Psr\Container\ContainerInterface;
+use Api\Infrastructure\Model\EventDispatcher\Listener;
 use Api\Infrastructure\Model\EventDispatcher\SyncEventDispatcher;
 use Api\Model\User as UserModel;
-use Api\Infrastructure\Model\EventDispatcher\Listener;
+use Api\Model\Video as VideoModel;
+use Psr\Container\ContainerInterface;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 return [
-    Api\Model\EventDispatcher::class =>
-        function (ContainerInterface $container) {
-            return new SyncEventDispatcher(
-                $container,
-                [
-                    UserModel\Entity\User\Event\UserCreated::class => [
-                        Listener\User\CreatedListener::class,
-                    ],
-                ]
-            );
-        },
+    Api\Model\EventDispatcher::class => function (ContainerInterface $container) {
+        return new SyncEventDispatcher(
+            $container,
+            [
+                UserModel\Entity\User\Event\UserCreated::class => [
+                    Listener\User\CreatedListener::class,
+                ],
+                VideoModel\Entity\Video\Event\VideoCreated::class => [
+                    Listener\Video\VideoCreatedListener::class,
+                ],
+            ]
+        );
+    },
 
-    Listener\User\CreatedListener::class =>
-        function (ContainerInterface $container) {
-            return new Listener\User\CreatedListener(
-                $container->get(Swift_Mailer::class),
-                $container->get('config')['mailer']['from']
-            );
-        }
+    Listener\User\CreatedListener::class => function (ContainerInterface $container) {
+        return new Listener\User\CreatedListener(
+            $container->get(Swift_Mailer::class),
+            $container->get('config')['mailer']['from']
+        );
+    },
+
+    Listener\Video\VideoCreatedListener::class => function (ContainerInterface $container) {
+        return new Listener\Video\VideoCreatedListener(
+            $container->get(AMQPStreamConnection::class)
+        );
+    },
 ];
